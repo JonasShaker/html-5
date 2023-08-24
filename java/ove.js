f60291f541f018028a67ecd673056006852a4270
Jonas <joab0001@stud.kea.dk>
2023-08-24 10:25:06 +0200

    java.con

---
+++ java/ove.js
@@ -0,0 +1,17 @@
+const h1 =  document.querySelector("h1");
+console.log(h1);
+
+const h2 = document.querySelector("h2");
+console.log(h2);
+
+const h3 = document.querySelector("h3");
+console.log(h3);
+
+const img = document.querySelector("img");
+console.log(img);
+
+const article = document.querySelector("article");
+console.log(article);
+
+
+
