ec3f8550fb467ef7238e0b902cb22c09b2884ba8
Jonas <joab0001@stud.kea.dk>
2023-08-24 11:03:13 +0200

    info

--- java/ove.js
+++ java/ove.js
@@ -1,11 +1,17 @@
 const h1 =  document.querySelector("h1");
 console.log(h1);
+const info = document.querySelector("h1");
+info.textContent= "HILS";
 
 const h2 = document.querySelector("h2");
 console.log(h2);
+const info2 = document.querySelector("h2");
+info2.textContent = "igos,ik";
 
 const h3 = document.querySelector("h3");
 console.log(h3);
+const info3 = document.querySelector("h3");
+info3.textContent ="jaaaaaaja";
 
 const img = document.querySelector("img");
 console.log(img);
